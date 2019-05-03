58010e1940b72f00a2d695d941d0b419f89d9dbd
Atakan Yuksel <atayuksel@outlook.com>
2019-05-03 11:51:10 +0300

    bug fix

--- driver.py
+++ driver.py
@@ -18,7 +18,7 @@ num_batch_in_epoch = len(data_interface.dataset['training']['data']) + 1
 # tensorflow placeholder
 data_ph = tf.placeholder(tf.int64, [batch_size, max_seq_length])
 labels_ph = tf.placeholder(tf.float32, [batch_size, 2])
-embedding_ph = tf.placeholder(tf.float32, [batch_size, embedding_dimension])
+embedding_ph = tf.placeholder(tf.float32, [vocabulary_size, embedding_dimension])
 seq_lens_ph = tf.placeholder(tf.int64, [batch_size, ])
 
 # network hyper-parameters
@@ -40,8 +40,8 @@ with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     word_embedding_init = model.embedding_v.assign(embedding_ph)
     sess.run(word_embedding_init, feed_dict={embedding_ph: embedding_matrix})
-    for epoch in num_epoch:
-        for batch in num_batch_in_epoch:
+    for epoch in range(num_epoch):
+        for batch in range(num_batch_in_epoch):
             batch_data, batch_labels, batch_seq_lens = data_interface.get_batch(dataset_type='training')
             sess.run(model.optimize, feed_dict={data_ph: batch_data,
                                                 labels_ph: batch_labels,
